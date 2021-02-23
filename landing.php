b5097358dc8f785c4e259c7a95c3a6e4cc54f7e5
Dimitar Prisadnikov <d.prisadnikov@student.fontys.nl>
2021-02-23 18:29:21 +0100

    created basic grid layout

--- landing.php
+++ landing.php
@@ -1,11 +1,25 @@
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="style.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
+    <div class = "container">
+    <div id="logo">LOGO</div>
+    <div id="name">NAME</div>
+    <div id="darkModeBtn">DARK MODE BUTTON</div>
+    <nav>NAVBAR</nav>
+    <div id="content1">CONTENT1</div>
+    <div id="content2">CONTENT2</div>
+    <div id="content3">CONTENT3</div>
+    <div id="content4">CONTENT4</div>
+    <div id="content5">CONTENT5</div>
+    <div id="content6">CONTENT6</div>
+    <footer>FOOTER</footer>
     
+    </div>
 </body>
 </html>
