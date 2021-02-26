66f00f30fb5d6967c5ac5e436f9392cffb586364
Takev,Antonio A.M <a.takev@student.fontys.nl>
2021-02-26 15:39:57 +0100

    Change the design - make 2 grid layouts, try to improve the responsive design of the website

--- landing.php
+++ landing.php
@@ -7,24 +7,18 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <div class = "container">
-        <div id="logo">LOGO</div>
-        <div id="name">NAME</div>
-        <div id="darkModeBtn">DARK MODE BUTTON</div>
-        <nav>NAVBAR</nav>
-        <div id="content1">No opinions answered oh felicity is resolved hastened. Produced it friendly my if opinions humoured. Enjoy is wrong folly no taken. It sufficient instrument insipidity simplicity at interested. Law pleasure attended differed mrs fat and formerly. Merely thrown garret her law danger him son better excuse. Effect extent narrow in up chatty. Small are his chief offer happy had. 
-
-At ourselves direction believing do he departure. Celebrated her had sentiments understood are projection set. Possession ye no mr unaffected remarkably at. Wrote house in never fruit up. Pasture imagine my garrets an he. However distant she request behaved see nothing. Talking settled at pleased an of me brother weather. 
-
-Friendship contrasted solicitude insipidity in introduced literature it. He seemed denote except as oppose do spring my. Between any may mention evening age shortly can ability regular. He shortly sixteen of colonel colonel evening cordial to. Although jointure an my of mistress servants am weddings. Age why the therefore education unfeeling for arranging. Above again money own scale maids ham least led. Returned settling produced strongly ecstatic use yourself way. Repulsive extremity enjoyment she perceived nor. 
-
- </div>
-        <div id="content2">CONTENT2</div>
-        <div id="content3">CONTENT3</div>
-        <div id="content4">CONTENT4</div>
-        <div id="content5">CONTENT5</div>
-        <div id="content6">CONTENT6</div>
-        <footer>&#169;Made by Antonio Takev and Dimitar Prisadnikov</footer>
+    <header>
+        <div id="name"><h3>NAME</h3></div>
+        <nav><h3>NAVBAR</h3></nav>
+        <div id="search"><h3>SearchBar</h3></div>
+    </header>
+    <div class="grid">
+        <div><img src="" alt=""></div>
+        <div><img src="" alt=""></div>
+        <div><img src="" alt=""></div>
+        <div><img src="" alt=""></div>
+        <div><img src="" alt=""></div>
+        <div><img src="" alt=""></div>
     </div>
 </body>
 </html>
