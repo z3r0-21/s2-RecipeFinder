9c2b4de14d8833b77e6cc36446021ac768ba9407
Takev,Antonio A.M <a.takev@student.fontys.nl>
2021-02-27 00:05:39 +0100

    Add searchBar to the website as well as design it for the different viewports

--- landing.php
+++ landing.php
@@ -5,13 +5,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Recipe website</title>
     <link rel="stylesheet" href="style.css">
+    <!-- for the icons (searchBar) -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
     <header>
         <div id="name"><h3>NAME</h3></div>
         <nav><h3>NAVBAR</h3></nav>
         <div id="mobileHeader"></div>
-        <div id="search"><h3>SearchBar</h3></div>
+        <div id="search">
+            <form action="/">
+                <input type="text" placeholder="Search.." name="search">
+                <button type="submit"><i class="fa fa-search"></i></button>
+            </form>
+        </div>
     </header>
     <div class="grid">
         <div><img src="" alt=""></div>
