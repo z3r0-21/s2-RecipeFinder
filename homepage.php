cd211cce55dbfa1d10a07bb2faf7d8ccb564ea71
Dimitar Prisadnikov <d.prisadnikov@student.fontys.nl>
2021-02-28 19:45:33 +0100

    made landing page

---
+++ homepage.php
@@ -0,0 +1,39 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recipe website</title>
+    <link rel="stylesheet" href="homepage-styles.css">
+    <!-- for the icons (searchBar) -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <header>
+        <div id="name"><h3>NAME</h3></div>
+        <nav>
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#recipes">Recipes</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#account">Account</a></li>
+            </ul>
+        </nav>
+        <div id="mobileHeader"></div>
+        <div id="search">
+            <form action="/">
+                <input type="text" placeholder="Search.." name="search">
+                <button type="submit"><i class="fa fa-search"></i></button>
+            </form>
+        </div>
+    </header>
+    <div class="grid">
+        <div><img src="" alt=""></div>
+        <div><img src="" alt=""></div>
+        <div><img src="" alt=""></div>
+        <div><img src="" alt=""></div>
+        <div><img src="" alt=""></div>
+        <div><img src="" alt=""></div>
+    </div>
+</body>
+</html>
--- landing.php
+++ landing.php
@@ -4,36 +4,21 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Recipe website</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="landing-styles.css">
     <!-- for the icons (searchBar) -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
     <header>
-        <div id="name"><h3>NAME</h3></div>
-        <nav>
-            <ul>
-                <li><a href="#home">Home</a></li>
-                <li><a href="#recipes">Recipes</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#account">Account</a></li>
-            </ul>
-        </nav>
-        <div id="mobileHeader"></div>
-        <div id="search">
-            <form action="/">
-                <input type="text" placeholder="Search.." name="search">
-                <button type="submit"><i class="fa fa-search"></i></button>
-            </form>
-        </div>
-    </header>
-    <div class="grid">
-        <div><img src="" alt=""></div>
-        <div><img src="" alt=""></div>
-        <div><img src="" alt=""></div>
-        <div><img src="" alt=""></div>
-        <div><img src="" alt=""></div>
-        <div><img src="" alt=""></div>
-    </div>
+            <div id="name"><h3>NAME</h3></div>
+            <nav>
+                <ul>
+                    <li><a href="#account">SIGN UP/IN</a></li>
+                </ul>
+            </nav>
+            <a href="#" class="landingButton">Discover recipes now</a>
+            <div id="label">label</div>
+            <div id="backgroundPicture"></div>
+    <footer>footer</footer>
 </body>
 </html>
