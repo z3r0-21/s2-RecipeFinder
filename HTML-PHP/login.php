f161f724d25bfb0b4ab63fd49e33f39587ea2e08
Takev,Antonio A.M <a.takev@student.fontys.nl>
2021-04-04 00:42:11 +0300

    Fix log in and register page elements' alignment

--- HTML-PHP/login.php
+++ HTML-PHP/login.php
@@ -4,40 +4,42 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LoginForm</title>
-    <link rel="stylesheet" href="../CSS/loginRegister-styles.css">
+    <link rel="stylesheet" href="../CSS/login-styles.css">
 </head>
 <body>
-    <div id="flexContainer">
-        <div class="form-parent">
-            <?php include '../Handling/login-handling.php'; ?>
-            <form method="post">
-                <fieldset>
-                    <legend>Log in</legend>
-                    <div class="avatar">
-                        <img src="../Images/avatarLogin.png" alt="Avatar" class="avatar" width="200" height="200">
-                    </div>
-                    <div class="form">
-                        <label for="email"><b>Email</b></label>
-                        <input type="text" placeholder="Enter email" name="email" required>
+    <div class="container">
+        <div id="flexContainer">
+            <div class="form-parent">
+                <?php include '../Handling/login-handling.php'; ?>
+                <form method="post">
+                    <fieldset>
+                        <legend>Log in</legend>
+                        <div class="avatar">
+                            <img src="../Images/avatarLogin.png" alt="Avatar" class="avatar" width="200" height="200">
+                        </div>
+                        <div class="form">
+                            <label for="email"><b>Email</b></label>
+                            <input type="text" placeholder="Enter email" name="email" required>
 
-                        <label for="password"><b>Password</b></label>
-                        <input type="password" placeholder="Enter password" name="password" required>
+                            <label for="password"><b>Password</b></label>
+                            <input type="password" placeholder="Enter password" name="password" required>
 
-                        <button type="submit">Login</button>
-                        <label><input type="checkbox" checked="checked" name="remember"> Remember me</label>
-                    </div>
-                    <div class="form">
-                        <button type="button" class="cancelbtn">Cancel</button>
-                        <span class="password">Forgot <a href="#">password?</a></span>
-                    </div>
-                </fieldset>
-            </form>
+                            <button type="submit">Login</button>
+                            <label><input type="checkbox" checked="checked" name="remember"> Remember me</label>
+                        </div>
+                        <div class="form">
+                            <button type="button" class="cancelbtn">Cancel</button>
+                            <span class="password">Forgot <a href="#">password?</a></span>
+                        </div>
+                    </fieldset>
+                </form>
 
 
-        </div>
-        <div class="register">
-            <h2>New to this website?</h2>
-            <a href="../HTML-PHP/register.html">Click here to create an account</a>
+            </div>
+            <div class="register">
+                <h2>New to this website?</h2>
+                <a href="../HTML-PHP/register.php">Click here to create an account</a>
+            </div>
         </div>
     </div>
 
